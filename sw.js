27de01c4e06261bd31918ec2fd1f551f8ec27c2b
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-03 16:21:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,42 @@
-const CACHE_NAME = 'dg-zeiterfassung-v3';
-
-const SHELL = [
-  './',
-  './index.html',
-  './manifest.json',
-  './dg_icon_192.png',
-  './dg_icon_512.png'
-];
+const CACHE_NAME = 'dg-zeiterfassung-v28';
+const APP_SHELL = ['./', './index.html', './manifest.json', './dg_icon_192.png', './dg_icon_512.png'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL))
-  );
   self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL).catch(() => {})));
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
 
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME)
-          .then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() =>
-        caches.match(event.request)
-          .then(cached => cached || caches.match('./index.html'))
-      )
-  );
+  event.respondWith((async () => {
+    try {
+      const response = await fetch(event.request, { cache: 'no-store' });
+
+      if (response && response.ok) {
+        const cache = await caches.open(CACHE_NAME);
+        cache.put(event.request, response.clone()).catch(() => {});
+      }
+
+      return response;
+    } catch (err) {
+      const cached = await caches.match(event.request);
+
+      if (cached) return cached;
+
+      if (event.request.mode === 'navigate') {
+        return (await caches.match('./index.html')) || (await caches.match('./'));
+      }
+
+      throw err;
+    }
+  })());
 });
