e3c5d1af1bd4b2a065cde59fe210f0b9c45e7696
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-04 16:14:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
-const CACHE_NAME = 'dg-zeiterfassung-v33';
-const APP_SHELL = ['./', './index.html', './manifest.json', './dg_icon_192.png', './dg_icon_512.png'];
+const CACHE_NAME = 'dg-zeiterfassung-v34';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './dg_icon_192.png',
+  './dg_icon_512.png'
+];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(APP_SHELL).catch(() => {}))
@@ -12,11 +19,13 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil((async () => {
     const keys = await caches.keys();
+
     await Promise.all(
       keys
-        .filter(k => k !== CACHE_NAME)
-        .map(k => caches.delete(k))
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
     );
+
     await self.clients.claim();
   })());
 });
@@ -26,22 +35,33 @@ self.addEventListener('fetch', event => {
 
   event.respondWith((async () => {
     try {
-      const response = await fetch(event.request, { cache: 'no-store' });
+      const response = await fetch(event.request, {
+        cache: 'no-store'
+      });
 
       if (response && response.ok) {
         const cache = await caches.open(CACHE_NAME);
-        cache.put(event.request, response.clone()).catch(() => {});
+
+        cache.put(
+          event.request,
+          response.clone()
+        ).catch(() => {});
       }
 
       return response;
+
     } catch (err) {
       const cached = await caches.match(event.request);
 
-      if (cached) return cached;
+      if (cached) {
+        return cached;
+      }
 
       if (event.request.mode === 'navigate') {
-        return (await caches.match('./index.html')) ||
-               (await caches.match('./'));
+        return (
+          (await caches.match('./index.html')) ||
+          (await caches.match('./'))
+        );
       }
 
       throw err;
