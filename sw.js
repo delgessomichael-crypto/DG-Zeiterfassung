c88483f3bf095010cd5b8f8c5d2c14c25760fbc2
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-17 15:09:00 +0200

    6.0.2: Service Worker Cache fuer strikte Backendpruefung aktualisieren

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME='dg-zeiterfassung-6-0-2-20260917a';
+const CACHE_NAME='dg-zeiterfassung-6-0-2-20260917b';
 const APP60='./app-6.0-runtime.js?v=20260916b';
-const BACKEND_COMPAT='./app-6.0-backend-compat.js?v=20260917b';
+const BACKEND_COMPAT='./app-6.0-backend-compat.js?v=20260917c';
 const UI60='./app-6.0-ui.js?v=20260917b';
 const FIX601='./app-6.0.1-fix.js?v=20260917c';
 const REM602='./app-6.0.2-reminders.js?v=20260917a';
