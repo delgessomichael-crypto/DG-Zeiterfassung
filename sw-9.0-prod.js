cdb7b1096861013fb964b44cd08f8995fabe2f5c
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-20 07:15:21 +0200

    Cache index in 9.0 service worker

--- sw-9.0-prod.js
+++ sw-9.0-prod.js
@@ -1,5 +1,5 @@
 /* DG Zeiterfassung 9.0 production service worker */
-const CACHE='dg-zeiterfassung-9.0-prod-20260920-1';
+const CACHE='dg-zeiterfassung-9.0-prod-20260920-2';
 const STATIC=[
   './app-5.0.css?v=20260918-800-prod1',
   './app-9.0.js?v=20260920-900-prod1',
