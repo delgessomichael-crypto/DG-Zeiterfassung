36e175309c31aaef4cac119bbbc680c336d60bf6
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-20 10:58:31 +0200

    Route payroll-9.0-final.js through DG App 10 Railway API

--- payroll-9.0-final.js
+++ payroll-9.0-final.js
@@ -3,7 +3,7 @@
 (function(){
 'use strict';
 const V='9.0';
-const BACKEND='https://script.google.com/macros/s/AKfycby2L3SMgh2RoGWsNRUp6o11g4iyZ8bgkSIGaAZPnBXCkJTkDDGF9aydn9vVKMB7kXsO/exec';
+const BACKEND='https://dg-app-10-api-production.up.railway.app/';
 const $=id=>document.getElementById(id);
 let resultObserver=null,installing=false;
 
