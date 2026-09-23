d0f2dfd7b56934c1281cdc32ddc5f97cc5488024
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-23 16:08:37 +0200

    Refresh PWA cache for final 10.0 title fix

--- sw-9.0-final.js
+++ sw-9.0-final.js
@@ -1,12 +1,12 @@
 /* DG Zeiterfassung 10.0 - Railway migration cache */
-const CACHE='dg-zeiterfassung-10.0-title-20260923-1610';
+const CACHE='dg-zeiterfassung-10.0-title-final-20260923-1620';
 const STATIC=[
   './index.html',
   './app-5.0.css?v=20260918-800-prod1',
-  './core-9.0-final.js?v=20260922-1015-whatsapp2',
+  './core-9.0-final.js?v=20260923-1620-v10-title-final',
   './customerflow-9.0-final.js?v=20260920-900-final1',
   './payroll-9.0-final.js?v=20260923-1610-v10-title',
-  './ui-9.0-final.js?v=20260923-1610-v10-title',
+  './ui-9.0-final.js?v=20260923-1620-v10-title-final',
   './dg_icon_192.png',
   './dg_icon_512.png',
   './manifest.json'
