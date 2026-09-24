424be5174a71c1557178ef2674854678bceb925e
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-24 05:30:15 +0200

    Rotate PWA cache for automatic Gmail sync

--- sw-9.0-final.js
+++ sw-9.0-final.js
@@ -1,9 +1,9 @@
 /* DG Zeiterfassung 10.0 - Railway migration cache */
-const CACHE='dg-zeiterfassung-10.0-final-clean2-20260923-2240';
+const CACHE='dg-zeiterfassung-10.0-gmail20-20260924-0525';
 const STATIC=[
   './index.html',
   './app-5.0.css?v=20260918-800-prod1',
-  './core-9.0-final.js?v=20260923-2240-final-clean2',
+  './core-9.0-final.js?v=20260924-0525-gmail20',
   './customerflow-9.0-final.js?v=20260920-900-final1',
   './payroll-9.0-final.js?v=20260923-2240-v10-final',
   './ui-9.0-final.js?v=20260923-1620-v10-title-final',
