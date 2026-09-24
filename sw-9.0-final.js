bb4ddf5ed20676c3f96cc5bc2a77359a28725a71
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-24 10:27:19 +0200

    Rotate service worker for final employee signature fix

--- sw-9.0-final.js
+++ sw-9.0-final.js
@@ -1,9 +1,9 @@
 /* DG Zeiterfassung 10.0 - Railway migration cache */
-const CACHE='dg-zeiterfassung-10.0-employee-transfer-20260924-1025';
+const CACHE='dg-zeiterfassung-10.0-signature-final-20260924-1032';
 const STATIC=[
   './index.html',
   './app-5.0.css?v=20260918-800-prod1',
-  './core-9.0-final.js?v=20260924-1025-employee-transfer-fix',
+  './core-9.0-final.js?v=20260924-1032-signature-final',
   './customerflow-9.0-final.js?v=20260920-900-final1',
   './payroll-9.0-final.js?v=20260923-2240-v10-final',
   './ui-9.0-final.js?v=20260923-1620-v10-title-final',
