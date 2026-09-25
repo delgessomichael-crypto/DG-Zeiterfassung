db8ee9ed84b5f826e5cce8b3ad3fc3b6c3154f46
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-25 15:12:06 +0200

    Rotate PWA cache for inspection camera update

--- sw-9.0-final.js
+++ sw-9.0-final.js
@@ -1,5 +1,5 @@
 /* DG Zeiterfassung 10.0 - Railway migration cache */
-const CACHE='dg-zeiterfassung-10.0-stable-runtime-20260924';
+const CACHE='dg-zeiterfassung-10.0-stable-runtime-20260925-camera-count';
 const STATIC=[
   './index.html',
   './app-5.0.css',
