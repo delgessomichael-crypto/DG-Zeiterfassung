440084967f74bcc39d02da1a8f4e659ae9a65225
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-25 16:20:16 +0200

    Cache offer trash and customer search UI

--- sw-9.0-final.js
+++ sw-9.0-final.js
@@ -1,11 +1,11 @@
 /* DG Zeiterfassung 10.0 - Railway migration cache */
-const CACHE='dg-zeiterfassung-10.0-stable-runtime-20260925-1555-buttons-images';
+const CACHE='dg-zeiterfassung-10.0-stable-runtime-20260925-trash-search';
 const STATIC=[
   './index.html',
   './app-5.0.css',
   './core-9.0-final.js',
   './employee-controls-10.0.js',
-  './inspection-quick-10.0.js',\n  './button-customize-10.0.js',
+  './inspection-quick-10.0.js',\n  './button-customize-10.0.js',\n  './offer-trash-search-10.0.js',
   './day-total-display-10.0.js',
   './customerflow-9.0-final.js',
   './payroll-9.0-final.js',
