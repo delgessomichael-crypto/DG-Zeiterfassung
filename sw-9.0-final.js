08eff23e8a515329fd3904045f16ddd70afea65f
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-25 18:52:36 +0200

    Rotate cache for full compact office layout

--- sw-9.0-final.js
+++ sw-9.0-final.js
@@ -1,5 +1,5 @@
 /* DG Zeiterfassung 10.0 - Railway migration cache */
-const CACHE='dg-zeiterfassung-10.0-stable-runtime-20260925-office-compact';
+const CACHE='dg-zeiterfassung-10.0-stable-runtime-20260925-office-compact4';
 const STATIC=[
   './index.html',
   './app-5.0.css',
