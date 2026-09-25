9d5b64df7bccf6783b5a61339e4f0fe24546ba87
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-25 17:46:38 +0200

    Refresh service worker cache for dashboard counter fixes

--- sw-9.0-final.js
+++ sw-9.0-final.js
@@ -1,5 +1,5 @@
 /* DG Zeiterfassung 10.0 - Railway migration cache */
-const CACHE='dg-zeiterfassung-10.0-stable-runtime-20260925-trash-search';
+const CACHE='dg-zeiterfassung-10.0-stable-runtime-20260925-search-counter';
 const STATIC=[
   './index.html',
   './app-5.0.css',
