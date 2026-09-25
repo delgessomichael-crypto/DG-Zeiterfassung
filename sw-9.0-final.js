e36b085fd3f9430bf2a86162f298b8171a655834
delgessomichael-crypto <delgessomichael@gmail.com>
2026-09-25 18:44:38 +0200

    Cache compact office runtime cleanly

--- sw-9.0-final.js
+++ sw-9.0-final.js
@@ -1,11 +1,14 @@
 /* DG Zeiterfassung 10.0 - Railway migration cache */
-const CACHE='dg-zeiterfassung-10.0-stable-runtime-20260925-offer-dedupe';
+const CACHE='dg-zeiterfassung-10.0-stable-runtime-20260925-office-compact';
 const STATIC=[
   './index.html',
   './app-5.0.css',
   './core-9.0-final.js',
   './employee-controls-10.0.js',
-  './inspection-quick-10.0.js',\n  './button-customize-10.0.js',\n  './offer-trash-search-10.0.js',
+  './inspection-quick-10.0.js',
+  './button-customize-10.0.js',
+  './offer-trash-search-10.0.js',
+  './office-compact-10.0.js',
   './day-total-display-10.0.js',
   './customerflow-9.0-final.js',
   './payroll-9.0-final.js',
